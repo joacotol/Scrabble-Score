fd41fdd70ed1e9762295ab6ba58b15ad139a1b43
joacotol <joacochrist@gmail.com>
2022-02-02 14:33:38 -0500

    Solution Found

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,11 +1,50 @@
 class Scrabble {
+    static int totalScore = 0;
+
+    public static void main(String[] args) {
+    }
 
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] splitUp = word.split("");
+        totalScore = 0;
+        for (String data : splitUp ) {
+            String lowerCase = data.toLowerCase();
+            if( lowerCase.equals("a") || lowerCase.equals("e") || lowerCase.equals("i") || lowerCase.equals("o") || lowerCase.equals("u") || lowerCase.equals("l") || lowerCase.equals("n") || lowerCase.equals("r") || lowerCase.equals("s") || lowerCase.equals("t") ) {
+                totalScore += 1;
+            }
+
+            if( lowerCase.equals("d") || lowerCase.equals("g") ) {
+                totalScore += 2;
+            }
+
+            if( lowerCase.equals("b") || lowerCase.equals("c") || lowerCase.equals("m") || lowerCase.equals("p") ) {
+                totalScore += 3;
+            }
+
+            if( lowerCase.equals("f") || lowerCase.equals("h") || lowerCase.equals("v") || lowerCase.equals("w") || lowerCase.equals("y")) {
+                totalScore += 4;
+            }
+
+            if( lowerCase.equals("k") ) {
+                totalScore += 5;
+            }
+
+            if( lowerCase.equals("j") || lowerCase.equals("x") ) {
+                totalScore += 8;
+            }
+
+            if( lowerCase.equals("q") || lowerCase.equals("z") ) {
+                totalScore += 10;
+            }
+
+        } 
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
+
+        return totalScore;
     }
 
 }
